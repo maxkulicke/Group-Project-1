130ec126d40b3297e7a0707d7c0e64cf28d27841
Max Kulicke <mkulicke@gmail.com>
2019-12-30 22:22:32 -0500

    got scriptProject.js to a pretty good place for now. Implemented master function (governor()) which will eventually take the user address input, parse it, and initiate all the ajax calling processes. for now, it's just a hard coded zip pass in argument into governor. the Zillow API is successfully using a hard coded address to get real estate info. one of the next steps is to build a skeleton function that can take values from user input forms and pass them into the zillow api URL. as for weather, i successfully streamlined the zip -> FIPS -> backTracker() -> weather data arrays process by actually using the ajax success callback process the way it is meant to be. still having the error of making calls too fast, but can tackle that next. good place for a commit.

--- ScriptProject.js
+++ ScriptProject.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-  // the NOAA API is where all of climate data comes from
+  // the NOAA API is where all of weather data comes from
   var NOAAtoken = "OBzsTvSdeIEAZDdTInysIDJSVQZdhKtx";
 
   // smartyStreets API is what we'll use to convert a zip code query from user into the
@@ -8,32 +8,58 @@ $(document).ready(function () {
   var smartyStreetsToken = "Tr2dL8zZULmwYqfpMw3W";
   var smartyStreetsID = "a0c54500-b299-d806-f742-6e5b1e339615";
 
+  // zillow api key
+  var ZWSID = "X1-ZWz1hjdyrt1k3v_8fetn";
+
+  // proxy because zillow doesn't accept CORS?
+  var proxy = "https://cors-anywhere.herokuapp.com/";
+
+  // test call
+  var zip = "19143";
+  governor(zip);
+
+  // governor is the master function, will be triggered by user onclick event,
+  // when they submit address. a function will need to be added to parse address forms
+  // just works with zip code for now (currently hardcoded)
+  function governor(zip) {
+    zillowRunner(zip);
+    weatherRunner(zip);
+  }
+
+  // returns the 4 character string for the current year.
+  // this return is used by backTracker() to have a year
+  // to work backwards from
   function getYear() {
     var now = parseInt(moment().format('YYYY'));
     return now;
   }
 
+  // initiates the zillow process with user input info
+  function zillowRunner(zip) {
+    zillowGetter();
+  }
+
+  // initiates the weather data process with user input info
+  function weatherRunner(zip) {
+    zipCaller(zip);
+  }
+
+  // queries smartyStreets API with zip code from user, passes
+  // the FIPS code pulled from the ajax response into backTracker()
   function zipCaller(zip) {
     var zipQueryUrl
       = "https://us-zipcode.api.smartystreets.com/lookup?auth-id=" + smartyStreetsID +
       "&auth-token=" + smartyStreetsToken + "&zipcode=" + zip;
     $.ajax({
       url: zipQueryUrl,
-      method: "GET"
-    }).then(function (response) {
-      // FIPSfinder(response);
-      console.log(response);
-      console.log(FIPSfinder(response));
-    });
+      method: "GET",
+      success: function (response) {
+        backTracker(response[0].zipcodes[0].county_fips);
+      }
+    })
   }
 
-  // test call
-  // zipCaller("19143");
-
-  function FIPSfinder(response) {
-    return response[0].zipcodes[0].county_fips;
-  }
-
+  // SAVE FOR LATER, needed to test error probing process
   // 400 ERROR test call
   // var testURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GSOY&datatypeid=TAVG&locationid=FIPS:06115&startdate=2010-01-01&enddate=2018-01-01";
   // $.ajax({
@@ -44,34 +70,13 @@ $(document).ready(function () {
   //   console.log(response);
   // });
 
-
-  // function noaaLocationList() {
-  //   var locationURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/locationcategories";
-  //   $.ajax({
-  //     url: locationURL,
-  //     headers: { token: NOAAtoken },
-  //     method: "GET"
-  //   }).then(function (response) {
-  //     console.log(response);
-  //   });
-  // }
-
-  // function noaaZIPlist() {
-  //   var zipURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?locationcategoryid=CNTY&sortfield=name&sortorder=desc&limit=1000";
-  //   $.ajax({
-  //     url: zipURL,
-  //     headers: { token: NOAAtoken },
-  //     method: "GET"
-  //   }).then(function (response) {
-  //     console.log(response);
-  //   });
-  // }
-
   // this function gets the yearly temp avg data points for a county code
   // it then crunches the numbers down into a single yearly avg which is
   // pushed into the beginning of the TAVG array
-  function noaaTAVG(startYear, endYear, array) {
-    var tavgURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GSOY&datatypeid=TAVG&locationid=FIPS:06115&startdate=" + startYear + "-01-01&enddate=" + endYear + "-01-01";
+  function noaaTAVG(FIPS, startYear, endYear, array) {
+    var tavgURL =
+      "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GSOY&datatypeid=TAVG&locationid=FIPS:"
+      + FIPS + "&startdate=" + startYear + "-01-01&enddate=" + endYear + "-01-01";
     $.ajax({
       url: tavgURL,
       headers: { token: NOAAtoken },
@@ -79,17 +84,16 @@ $(document).ready(function () {
       success: function (response) {
         ajaxDataMaker(response, array);
       }
-      // }).then(function (response) {
-      //   ajaxDataMaker(response, array);
-      // });
     });
   }
 
   // this function gets the yearly temp maximum data points for a county code
   // it then crunches the numbers down into a single yearly avg which is
   // pushed into the beginning of the TMAX array
-  function noaaTMAX(startYear, endYear, array) {
-    var tmaxURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GSOY&datatypeid=TMAX&locationid=FIPS:42101&startdate=" + startYear + "-01-01&enddate=" + endYear + "-01-01";
+  function noaaTMAX(FIPS, startYear, endYear, array) {
+    var tmaxURL =
+      "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GSOY&datatypeid=TMAX&locationid=FIPS:"
+      + FIPS + "&startdate=" + startYear + "-01-01&enddate=" + endYear + "-01-01";
     $.ajax({
       url: tmaxURL,
       headers: { token: NOAAtoken },
@@ -102,8 +106,10 @@ $(document).ready(function () {
   // this function gets the yearly precipitation total data points for a county code
   // it then crunches the numbers down into a single yearly avg which is
   // pushed into the beginning of the PRCP array
-  function noaaPRCP(startYear, endYear, array) {
-    var prcpURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GSOY&datatypeid=PRCP&locationid=FIPS:42101&startdate=" + startYear + "-01-01&enddate=" + endYear + "-01-01";
+  function noaaPRCP(FIPS, startYear, endYear, array) {
+    var prcpURL =
+      "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GSOY&datatypeid=PRCP&locationid=FIPS:"
+      + FIPS + "&startdate=" + startYear + "-01-01&enddate=" + endYear + "-01-01";
     $.ajax({
       url: prcpURL,
       headers: { token: NOAAtoken },
@@ -118,6 +124,7 @@ $(document).ready(function () {
   // then pushes them to the beginning of the respective array
   function ajaxDataMaker(response, array) {
     var yearResults = [];
+    // forEach?
     for (var i = 0; i < response.results.length; i++) {
       yearResults.push((response.results[i].value));
     }
@@ -125,19 +132,24 @@ $(document).ready(function () {
     arrayMaker(yearResultsAvg, array);
   };
 
-  function backTracker() {
+  // backTracker takes the FIPS argument and works backwards through time,
+  // calling the ajax caller functions to add data for each year.
+  function backTracker(FIPS) {
     var TAVG = [];
     var TMAX = [];
     var PRCP = [];
     var startYear = getYear() - 2;
     // should take start year from getNow year - 2;
+    // need to go backwards until error.....
+    // should backwards loop be separate function?
     for (var i = 0; i < 10; i++) {
       var endYear = startYear + 1;
-      noaaTAVG(startYear, endYear, TAVG);
-      noaaTMAX(startYear, endYear, TMAX);
-      noaaPRCP(startYear, endYear, PRCP);
+      noaaTAVG(FIPS, startYear, endYear, TAVG);
+      noaaTMAX(FIPS, startYear, endYear, TMAX);
+      noaaPRCP(FIPS, startYear, endYear, PRCP);
       startYear--;
     }
+    // logs should be deleted at some point
     console.log(TAVG);
     console.log(TMAX);
     console.log(PRCP);
@@ -157,8 +169,71 @@ $(document).ready(function () {
     return arrayAvg;
   }
 
-  // test call
-  // backTracker();
+  // zillow stuff
+
+  // zillow api key
+  var ZWSID = "X1-ZWz1hjdyrt1k3v_8fetn";
+
+  // need a function that can parse the user input into a string with '+' instead of a space
+
+  function zillowGetter() {
+    // // proxy because zillow doesn't accept CORS?
+    // var proxy = "https://cors-anywhere.herokuapp.com/";
+    var zillowURL = "https://www.zillow.com/webservice/GetSearchResults.htm?zws-id="
+      + ZWSID + "&address=4922+Warrington+Ave&citystatezip=Philadelphia%2C+PA+19143";
+    $.ajax({
+      url: proxy + zillowURL,
+      method: "GET"
+    }).then(function (response) {
+      var JSONresponse = xmlToJson(response);
+      console.log(JSONresponse);
+    });
+  }
+
+  // NOTE! the following function is copied verbatim from this website:
+  // https://davidwalsh.name/convert-xml-json
+  // the zillow API returns xml as default. until i can figure out the proper
+  // way to call for a JSON output, I will use this function to convert the
+  // XML to JSON for ease of access/traversing
+
+  // Changes XML to JSON
+  function xmlToJson(xml) {
+
+    // Create the return object
+    var obj = {};
+
+    if (xml.nodeType == 1) { // element
+      // do attributes
+      if (xml.attributes.length > 0) {
+        obj["@attributes"] = {};
+        for (var j = 0; j < xml.attributes.length; j++) {
+          var attribute = xml.attributes.item(j);
+          obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+        }
+      }
+    } else if (xml.nodeType == 3) { // text
+      obj = xml.nodeValue;
+    }
+
+    // do children
+    if (xml.hasChildNodes()) {
+      for (var i = 0; i < xml.childNodes.length; i++) {
+        var item = xml.childNodes.item(i);
+        var nodeName = item.nodeName;
+        if (typeof (obj[nodeName]) == "undefined") {
+          obj[nodeName] = xmlToJson(item);
+        } else {
+          if (typeof (obj[nodeName].push) == "undefined") {
+            var old = obj[nodeName];
+            obj[nodeName] = [];
+            obj[nodeName].push(old);
+          }
+          obj[nodeName].push(xmlToJson(item));
+        }
+      }
+    }
+    return obj;
+  };
 
 })
 
